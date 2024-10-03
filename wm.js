8b19f343f31a07c795016907af536ee7a26b183f
Yasmin Matos <152352014+matosyas@users.noreply.github.com>
2024-10-02 22:50:43 -0300

    Update wm.js

---
+++ wm.js
@@ -0,0 +1,17 @@
+var swiper = new Swiper(".wmSlider", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    loop: true,
+    autoplay: {
+        delay: 4500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
